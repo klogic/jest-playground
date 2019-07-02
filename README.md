4dd300e53b4177a98a622e79e58a6fd8782a433c
Narongsak Keawmanee <klogic@hotmail.co.th>
2019-07-02 21:07:02 +0700

    integrate Typescript with ts-jest

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # About this project
 
 This project playground for leaning [Jest](https://jestjs.io/docs/en/getting-started)
+Impliment with ts-jest for support Typescript
+
+## Command
+
+```
+yarn test
+```
---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  roots: ["<rootDir>/"],
+  testMatch: ["<rootDir>/**/*.test.ts"],
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json"],
+  testEnvironment: "node"
+};
